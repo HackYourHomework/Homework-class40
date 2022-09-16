4b336740c48e2252390e287bfc81fc54aaa8b614
mysr3809 <myasar0938@gmail.com>
2022-09-16 08:48:39 +0200

    second exercise done

--- 1-JavaScript/Week3/homework/ex2-dogYears.js
+++ 1-JavaScript/Week3/homework/ex2-dogYears.js
@@ -16,18 +16,20 @@ calculate it!
    ages.
 -----------------------------------------------------------------------------*/
 
-function calculateDogAge(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function calculateDogAge(dogAge) {
+    // TODO complete this function
+    const result = `Your doggie is ${(dogAge*7)} years old in dog years!`;
+    return result;
 }
 
 function main() {
-  console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
-  console.log(calculateDogAge(2)); // -> "Your doggie is 14 years old in dog years!"
-  console.log(calculateDogAge(3)); // -> "Your doggie is 21 years old in dog years!"
+    console.log(calculateDogAge(1)); // -> "Your doggie is 7 years old in dog years!"
+    console.log(calculateDogAge(2)); // -> "Your doggie is 14 years old in dog years!"
+    console.log(calculateDogAge(3)); // -> "Your doggie is 21 years old in dog years!"
 }
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
-  main();
+    main();
 }
 module.exports = calculateDogAge;
