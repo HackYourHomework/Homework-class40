8e050c21257d3bf10ce57811b91a079613731dc8
trebektas <trensarbektas@gmail.com>
2022-09-27 14:33:39 +0200

    ex4 and ex5 is finished

--- 1-JavaScript/Week4/homework/ex5-wallet/index.js
+++ 1-JavaScript/Week4/homework/ex5-wallet/index.js
@@ -1,5 +1,9 @@
 'use strict';
+const config = {
+  sandboxInjectedGlobals: ['Math'],
+};
 
+module.exports = config;
 // Based on an example from: Philipp Beau (@ze_german)
 
 const eurosFormatter = new Intl.NumberFormat('nl-NL', {
@@ -24,9 +28,9 @@ function createWallet(name, cash = 0) {
 
   function transferInto(wallet, amount) {
     console.log(
-      `Transferring ${eurosFormatter.format(amount)} from ${name} to ${
-        wallet.name
-      }`
+      `Transferring ${eurosFormatter.format(
+        amount
+      )} from ${name} to ${wallet.getName()}`
     );
     const withdrawnAmount = withdraw(amount);
     wallet.deposit(withdrawnAmount);
@@ -77,7 +81,7 @@ const quiz = {
       b: 'cash, name', 
       c: 'amount, this, wallet'
     },
-    answer: undefined,
+    answer: 'b',
   },
   q2: {
     question: 'What is in the Call Stack, from top to bottom?',
@@ -86,7 +90,7 @@ const quiz = {
       b: 'anonymous, transferInto', 
       c: 'transferInto, anonymous' 
     },
-    answer: undefined,
+    answer: 'c',
   },
   q3: {
     question: 'What tooltip appears when hovering over the third debug button?',
@@ -95,7 +99,7 @@ const quiz = {
       b: 'Step out of current function', 
       c: 'Step' 
     },
-    answer: undefined,
+    answer: 'a',
   },
   q4: {
     question: 'What is displayed in the console?',
@@ -104,7 +108,7 @@ const quiz = {
       b: 'Transferring € 50,00 from Jack to undefined', 
       c: 'Transferring € 50,00 from Jack to Jane' 
     },
-    answer: undefined,
+    answer: 'a',
   },
   q5: {
     question: 'The owner of the wallet with insufficient funds is:',
@@ -113,6 +117,6 @@ const quiz = {
       b: 'Joe', 
       c: 'Jane' 
     },
-    answer: undefined,
+    answer: 'c',
   },
 };
