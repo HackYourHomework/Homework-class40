6144d188c34a8fc42852c73235a492b491c0b379
Alqayadhi <mo.alqayadhi@gmail.com>
2022-10-28 20:21:36 +0200

    here are the two weeks

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -18,7 +18,27 @@ https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 //cspell: enable
 
 function createBookList(books) {
-  // TODO your code goes in here, return the ul element
+  const bookList = document.createElement('ul');
+
+  books.forEach((book) => {
+    const ilElement = document.createElement('li');
+    const paragraphElement = document.createElement('p');
+    const imageElement = document.createElement('img');
+    imageElement.classList.add('bookImage');
+
+    bookList.appendChild(ilElement);
+
+    ilElement.appendChild(paragraphElement);
+
+    paragraphElement.textContent = `${book.title} - ${book.author}`;
+
+    ilElement.appendChild(imageElement);
+    imageElement.src = `./assets/${book.link}`;
+    imageElement.alt = book.link;
+
+    ilElement.style.background = book.alreadyRead ? 'green' : 'red';
+  });
+  return bookList;
 }
 
 function main() {
@@ -28,18 +48,21 @@ function main() {
       author: 'Don Norman',
       isbn: '978-0465050659',
       alreadyRead: false,
+      link: 'the_design_of_everyday_things.jpg',
     },
     {
       title: 'The Most Human Human',
       author: 'Brian Christian',
       isbn: '978-1617933431',
       alreadyRead: true,
+      link: 'the_most_human_human.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
       author: 'Andrew Hunt',
       isbn: '978-0201616224',
       alreadyRead: true,
+      link: 'the_pragmatic_programmer.jpg',
     },
   ];
 
--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,8 +7,26 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
   second). Use `setInterval()` to make sure the time stays current.
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
-function addCurrentTime() {
-  // TODO complete this function
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i;
+  }
+  return i;
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+function addCurrentTime() {
+  const today = new Date();
+
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+
+  const currentTime = `${h}:${m}:${s}`;
+  return currentTime;
+}
+
+window.onload = () => setInterval(addCurrentTime, 1000);
