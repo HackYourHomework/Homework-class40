fda90b1810ec62b1c6e598d190c3dbfa3514a164
trebektas <trensarbektas@gmail.com>
2022-10-29 09:06:25 +0200

    Add variable for dice map and give the name as 'dices'

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -28,7 +28,8 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
   const dice = [1, 2, 3, 4, 5];
-  return Promise.all(dice.map((die) => rollDie(die)));
+  const dices = dice.map(rollDie);
+  return Promise.all(dices);
 }
 
 function main() {
