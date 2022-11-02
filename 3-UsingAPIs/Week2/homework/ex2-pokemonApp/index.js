b0d4313bf9b942c00c19a2d85cfe53b3798bf7f9
Alan <alanhussein116@gmail.com>
2022-11-02 18:42:49 +0100

    ex2

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,18 +22,60 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw 'error';
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchAndPopulatePokemons(data) {
+  const pokemonData = await fetchData(data);
+  const container = document.createElement('div');
+  const btn = document.createElement('button');
+  btn.setAttribute('type', 'button');
+  btn.textContent = 'Get Pokemon';
+  const select = document.createElement('select');
+  btn.addEventListener('click', () => {
+    pokemonData.results.forEach((element) => {
+      const { name, url } = element;
+      const option = document.createElement('option');
+      option.text = name;
+      option.value = url;
+      select.appendChild(option);
+    });
+    select.addEventListener('change', (el) => {
+      fetchImage(el.target.value, container);
+    });
+  });
+  document.body.appendChild(btn);
+  document.body.appendChild(select);
+  document.body.appendChild(container);
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(url, container) {
+  const imageSrc = await fetchData(url);
+  const img = document.createElement('img');
+  img.src = imageSrc.sprites.front_default;
+  img.alt = imageSrc.name;
+  container.textContent = ' ';
+  container.appendChild(img);
 }
 
 function main() {
-  // TODO complete this function
+  try {
+    return fetchAndPopulatePokemons(
+      'https://pokeapi.co/api/v2/pokemon?limit=151'
+    );
+  } catch (err) {
+    console.log(err);
+  }
 }
+main();
